acdd2b31f48ecff5a41f219e6b3f35af597588f9
dmitry mansurov <dimamansurov@gmail.com>
2023-03-15 17:00:52 +0300

    fix the for..of loop for index.js, add asciinema

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/d-mansurov/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/d-mansurov/frontend-project-46/actions)
+
+Asciinema https://asciinema.org/a/UdOcNlOBlT5kBGFaoQcQNSl0S
--- src/index.js
+++ src/index.js
@@ -1,31 +1,42 @@
 import { readFileSync } from 'fs';
 import _ from 'lodash';
+import path from 'path';
+
+// Function that reads files with absolute and relative paths:
+const readFile = (filePath) => {
+  const fullPath = path.resolve(process.cwd(), filePath);
+  const fileData = readFileSync(fullPath.toString());
+  return fileData;
+};
 
 const genDiff = (data1, data2) => {
   const keys1 = _.keys(data1);
   const keys2 = _.keys(data2);
-  const keys = _.union(keys1, keys2);
+  const keys = _.sortBy(_.union(keys1, keys2));
 
-  const result = {};
-  for (const key of keys) {
+  const result = keys.map((key) => {
     if (!_.has(data1, key)) {
-      result[`+ ${key}`] = data2[key];
+      return [`+ ${key}: ${data2[key]}`];
     } else if (!_.has(data2, key)) {
-      result[`- ${key}`] = data1[key];
+      return [`- ${key}: ${data1[key]}`];
     } else if (data1[key] !== data2[key]) {
-      result[`- ${key}`] = data1[key];
-      result[`+ ${key}`] = data2[key];
-    } else if (data1[key] === data2[key]) {
-      result[`  ${key}`] = data1[key];
+      return [`- ${key}: ${data1[key]}`, `+ ${key}: ${data2[key]}`];
+    } else {
+      return [`  ${key}: ${data1[key]}`];
     }
-  }
+  });
 
-  return JSON.stringify(result, null, 2);
+  const flatResult = result.flat();
+  const objectResult = Object.fromEntries(flatResult.map((entry) => entry.split(': ')));
+
+  const diffString = JSON.stringify(objectResult, null, 2);
+  return diffString.split('"').join('');
 };
 
 export default (filePath1, filePath2) => {
-  const fileData1 = readFileSync(filePath1, 'utf-8');
-  const fileData2 = readFileSync(filePath2, 'utf-8');
+  const fileData1 = readFile(filePath1);
+  const fileData2 = readFile(filePath2);
+
   const fileDataObject1 = JSON.parse(fileData1);
   const fileDataObject2 = JSON.parse(fileData2);
 
